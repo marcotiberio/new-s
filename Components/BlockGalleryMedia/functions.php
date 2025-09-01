ab76ab71be6ccfa84b0cbcefd8221a9f60789122
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2025-09-01 13:31:05 +0200

    added vimeo embed policy

--- Components/BlockGalleryMedia/functions.php
+++ Components/BlockGalleryMedia/functions.php
@@ -3,6 +3,27 @@
 namespace Flynt\Components\BlockGalleryMedia;
 
 use Flynt\FieldVariables;
+use Flynt\Utils\Oembed;
+
+add_filter('Flynt/addComponentData?name=BlockGalleryMedia', function ($data) {
+    if (isset($data['mediaItems']) && is_array($data['mediaItems'])) {
+        foreach ($data['mediaItems'] as &$item) {
+            if (isset($item['acf_fc_layout']) && $item['acf_fc_layout'] === 'oembed' && isset($item['oembed'])) {
+                $item['oembed'] = Oembed::setSrcAsDataAttribute(
+                    $item['oembed'],
+                    [
+                        'autoplay' => 'true',
+                        'loop' => 'true',
+                        'muted' => 'true',
+                        'controls' => 'false'
+                    ]
+                );
+            }
+        }
+    }
+
+    return $data;
+});
 
 function getACFLayout()
 {
@@ -178,13 +199,71 @@ function getACFLayout()
                         ]
                     ],
                     [
-                        'name' => 'video_embed',
+                        'name' => 'oembed',
                         'label' => __('Video Embed', 'flynt'),
                         'display' => 'block',
                         'sub_fields' => [
                             [
+                                'label' => __('Text starts in column:', 'flynt'),
+                                'name' => 'colStart',
+                                'type' => 'button_group',
+                                'choices' => [
+                                    'col-start-1 lg:col-start-1' => sprintf('<p>1</p>', __('1', 'flynt')),
+                                    'col-start-1 lg:col-start-2' => sprintf('<p>2</p>', __('2', 'flynt')),
+                                    'col-start-1 lg:col-start-3' => sprintf('<p>3</p>', __('3', 'flynt')),
+                                    'col-start-1 lg:col-start-4' => sprintf('<p>4</p>', __('4', 'flynt')),
+                                    'col-start-1 lg:col-start-5' => sprintf('<p>5</p>', __('5', 'flynt')),
+                                    'col-start-1 lg:col-start-6' => sprintf('<p>6</p>', __('6', 'flynt')),
+                                    'col-start-1 lg:col-start-7' => sprintf('<p>7</p>', __('7', 'flynt')),
+                                    'col-start-1 lg:col-start-8' => sprintf('<p>8</p>', __('8', 'flynt')),
+                                    'col-start-1 lg:col-start-9' => sprintf('<p>9</p>', __('9', 'flynt')),
+                                    'col-start-1 lg:col-start-10' => sprintf('<p>10</p>', __('10', 'flynt')),
+                                    'col-start-1 lg:col-start-11' => sprintf('<p>11</p>', __('11', 'flynt')),
+                                    'col-start-1 lg:col-start-12' => sprintf('<p>12</p>', __('12', 'flynt'))
+                                ],
+                                'wrapper' => [
+                                    'width' => 33
+                                ],
+                            ],
+                            [
+                                'label' => __('Text ends in column:', 'flynt'),
+                                'name' => 'colEnd',
+                                'type' => 'button_group',
+                                'choices' => [
+                                    'col-end-4 lg:col-end-1' => sprintf('<p>1</p>', __('1', 'flynt')),
+                                    'col-end-4 lg:col-end-2' => sprintf('<p>2</p>', __('2', 'flynt')),
+                                    'col-end-4 lg:col-end-3' => sprintf('<p>3</p>', __('3', 'flynt')),
+                                    'col-end-4 lg:col-end-4' => sprintf('<p>4</p>', __('4', 'flynt')),
+                                    'col-end-4 lg:col-end-5' => sprintf('<p>5</p>', __('5', 'flynt')),
+                                    'col-end-4 lg:col-end-6' => sprintf('<p>6</p>', __('6', 'flynt')),
+                                    'col-end-4 lg:col-end-7' => sprintf('<p>7</p>', __('7', 'flynt')),
+                                    'col-end-4 lg:col-end-8' => sprintf('<p>8</p>', __('8', 'flynt')),
+                                    'col-end-4 lg:col-end-9' => sprintf('<p>9</p>', __('9', 'flynt')),
+                                    'col-end-4 lg:col-end-10' => sprintf('<p>10</p>', __('10', 'flynt')),
+                                    'col-end-4 lg:col-end-11' => sprintf('<p>11</p>', __('11', 'flynt')),
+                                    'col-end-4 lg:col-end-13' => sprintf('<p>12</p>', __('12', 'flynt')),
+                                ],
+                                'wrapper' => [
+                                    'width' => 33
+                                ],
+                            ],
+                            [
+                                'label' => __('Align Vertically', 'flynt'),
+                                'name' => 'alignY',
+                                'type' => 'button_group',
+                                'choices' => [
+                                    'lg:items-start' => sprintf('<p>Top</p>', __('Top', 'flynt')),
+                                    'lg:items-center' => sprintf('<p>Center</p>', __('Center', 'flynt')),
+                                    'lg:items-end' => sprintf('<p>Bottom</p>', __('Bottom', 'flynt')),
+                                ],
+                                'default_value' => 'lg:items-start',
+                                'wrapper' => [
+                                    'width' => 33
+                                ],
+                            ],
+                            [
                                 'label' => __('Video Embed', 'flynt'),
-                                'name' => 'videoEmbed',
+                                'name' => 'oembed',
                                 'type' => 'oembed',
                             ]
                         ]
