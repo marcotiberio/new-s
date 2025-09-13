81ed6bf7b7709eb26dd23a1d49ff4671502447ce
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2025-09-13 10:54:23 +0200

    fix projects grid issue

--- Components/ListingProjects/functions.php
+++ Components/ListingProjects/functions.php
@@ -65,49 +65,63 @@ function getACFLayout()
                         ]
                     ],
                     [
-                        'label' => __('Project starts in column:', 'flynt'),
-                        'name' => 'colStart',
+                        'label' => __('Project Width', 'flynt'),
+                        'name' => 'width',
                         'type' => 'button_group',
                         'choices' => [
-                            'col-start-1 lg:col-start-1' => sprintf('<p>1</p>', __('1', 'flynt')),
-                            'col-start-1 lg:col-start-2' => sprintf('<p>2</p>', __('2', 'flynt')),
-                            'col-start-1 lg:col-start-3' => sprintf('<p>3</p>', __('3', 'flynt')),
-                            'col-start-1 lg:col-start-4' => sprintf('<p>4</p>', __('4', 'flynt')),
-                            'col-start-1 lg:col-start-5' => sprintf('<p>5</p>', __('5', 'flynt')),
-                            'col-start-1 lg:col-start-6' => sprintf('<p>6</p>', __('6', 'flynt')),
-                            'col-start-1 lg:col-start-7' => sprintf('<p>7</p>', __('7', 'flynt')),
-                            'col-start-1 lg:col-start-8' => sprintf('<p>8</p>', __('8', 'flynt')),
-                            'col-start-1 lg:col-start-9' => sprintf('<p>9</p>', __('9', 'flynt')),
-                            'col-start-1 lg:col-start-10' => sprintf('<p>10</p>', __('10', 'flynt')),
-                            'col-start-1 lg:col-start-11' => sprintf('<p>11</p>', __('11', 'flynt')),
-                            'col-start-1 lg:col-start-12' => sprintf('<p>12</p>', __('12', 'flynt'))
+                            'w-full' => sprintf('<p>Full</p>', __('Full', 'flynt')),
+                            'w-full lg:w-[calc(50%_-_15px)]' => sprintf('<p>Half</p>', __('Half', 'flynt')),
+                            'w-full lg:w-[calc(33.33%_-_15px)]' => sprintf('<p>Third</p>', __('Third', 'flynt')),
+                            'w-full lg:w-[calc(25%_-_15px)]' => sprintf('<p>Quarter</p>', __('Quarter', 'flynt')),
                         ],
                         'wrapper' => [
                             'width' => 50
                         ],
                     ],
-                    [
-                        'label' => __('Project ends in column:', 'flynt'),
-                        'name' => 'colEnd',
-                        'type' => 'button_group',
-                        'choices' => [
-                            'col-end-13 lg:col-end-1' => sprintf('<p>1</p>', __('1', 'flynt')),
-                            'col-end-13 lg:col-end-2' => sprintf('<p>2</p>', __('2', 'flynt')),
-                            'col-end-13 lg:col-end-3' => sprintf('<p>3</p>', __('3', 'flynt')),
-                            'col-end-13 lg:col-end-4' => sprintf('<p>4</p>', __('4', 'flynt')),
-                            'col-end-13 lg:col-end-5' => sprintf('<p>5</p>', __('5', 'flynt')),
-                            'col-end-13 lg:col-end-6' => sprintf('<p>6</p>', __('6', 'flynt')),
-                            'col-end-13 lg:col-end-7' => sprintf('<p>7</p>', __('7', 'flynt')),
-                            'col-end-13 lg:col-end-8' => sprintf('<p>8</p>', __('8', 'flynt')),
-                            'col-end-13 lg:col-end-9' => sprintf('<p>9</p>', __('9', 'flynt')),
-                            'col-end-13 lg:col-end-10' => sprintf('<p>10</p>', __('10', 'flynt')),
-                            'col-end-13 lg:col-end-11' => sprintf('<p>11</p>', __('11', 'flynt')),
-                            'col-end-13 lg:col-end-13' => sprintf('<p>12</p>', __('12', 'flynt')),
-                        ],
-                        'wrapper' => [
-                            'width' => 50
-                        ],
-                    ],
+                    // [
+                    //     'label' => __('Project starts in column:', 'flynt'),
+                    //     'name' => 'colStart',
+                    //     'type' => 'button_group',
+                    //     'choices' => [
+                    //         'col-start-1 lg:col-start-1' => sprintf('<p>1</p>', __('1', 'flynt')),
+                    //         'col-start-1 lg:col-start-2' => sprintf('<p>2</p>', __('2', 'flynt')),
+                    //         'col-start-1 lg:col-start-3' => sprintf('<p>3</p>', __('3', 'flynt')),
+                    //         'col-start-1 lg:col-start-4' => sprintf('<p>4</p>', __('4', 'flynt')),
+                    //         'col-start-1 lg:col-start-5' => sprintf('<p>5</p>', __('5', 'flynt')),
+                    //         'col-start-1 lg:col-start-6' => sprintf('<p>6</p>', __('6', 'flynt')),
+                    //         'col-start-1 lg:col-start-7' => sprintf('<p>7</p>', __('7', 'flynt')),
+                    //         'col-start-1 lg:col-start-8' => sprintf('<p>8</p>', __('8', 'flynt')),
+                    //         'col-start-1 lg:col-start-9' => sprintf('<p>9</p>', __('9', 'flynt')),
+                    //         'col-start-1 lg:col-start-10' => sprintf('<p>10</p>', __('10', 'flynt')),
+                    //         'col-start-1 lg:col-start-11' => sprintf('<p>11</p>', __('11', 'flynt')),
+                    //         'col-start-1 lg:col-start-12' => sprintf('<p>12</p>', __('12', 'flynt'))
+                    //     ],
+                    //     'wrapper' => [
+                    //         'width' => 50
+                    //     ],
+                    // ],
+                    // [
+                    //     'label' => __('Project ends in column:', 'flynt'),
+                    //     'name' => 'colEnd',
+                    //     'type' => 'button_group',
+                    //     'choices' => [
+                    //         'col-end-13 lg:col-end-1' => sprintf('<p>1</p>', __('1', 'flynt')),
+                    //         'col-end-13 lg:col-end-2' => sprintf('<p>2</p>', __('2', 'flynt')),
+                    //         'col-end-13 lg:col-end-3' => sprintf('<p>3</p>', __('3', 'flynt')),
+                    //         'col-end-13 lg:col-end-4' => sprintf('<p>4</p>', __('4', 'flynt')),
+                    //         'col-end-13 lg:col-end-5' => sprintf('<p>5</p>', __('5', 'flynt')),
+                    //         'col-end-13 lg:col-end-6' => sprintf('<p>6</p>', __('6', 'flynt')),
+                    //         'col-end-13 lg:col-end-7' => sprintf('<p>7</p>', __('7', 'flynt')),
+                    //         'col-end-13 lg:col-end-8' => sprintf('<p>8</p>', __('8', 'flynt')),
+                    //         'col-end-13 lg:col-end-9' => sprintf('<p>9</p>', __('9', 'flynt')),
+                    //         'col-end-13 lg:col-end-10' => sprintf('<p>10</p>', __('10', 'flynt')),
+                    //         'col-end-13 lg:col-end-11' => sprintf('<p>11</p>', __('11', 'flynt')),
+                    //         'col-end-13 lg:col-end-13' => sprintf('<p>12</p>', __('12', 'flynt')),
+                    //     ],
+                    //     'wrapper' => [
+                    //         'width' => 50
+                    //     ],
+                    // ],
                 ],
             ],
             [
