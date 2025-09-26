18df4cf7cf3077f33b5c8c6c92f43310e1b5eff6
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2025-09-26 08:41:07 +0200

    add fade-in move-up animations

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,5 +1,45 @@
 import $ from 'jquery'
 import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+// Fade-in Animation
+gsap.registerPlugin(ScrollTrigger)
+
+// responsive
+const mm = gsap.matchMedia()
+
+window.onload = () => {
+  // scroll to element id on load
+  setTimeout(() => {
+    const url = window.location.href
+    const hash = url.substring(url.indexOf('#') + 1)
+    if (hash) {
+      const element = document.getElementById(hash)
+      if (element) {
+        element.scrollIntoView()
+      }
+    }
+  }, 200)
+}
+
+mm.add('(min-width: 1280px)', () => {
+  gsap.set('.fade-in', { opacity: 0 })
+  gsap.set('.move-up', { y: 200 })
+})
+
+ScrollTrigger.batch('.fade-in', {
+  onEnter: (elements) =>
+    gsap.to(elements, { opacity: 1, stagger: 0.1, duration: 0.5 }),
+  start: '1vh bottom',
+  end: 'top top',
+})
+
+ScrollTrigger.batch('.move-up', {
+  onEnter: (elements) =>
+    gsap.to(elements, { y: 0, stagger: 0.1, duration: 0.3 }),
+  start: '1vh bottom',
+  end: 'top top',
+})
 
 // Page Load Animation - Similar to Justified Studio
 class PageLoadAnimation {
