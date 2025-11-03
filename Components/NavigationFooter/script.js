3ce695146192b8941b5b73f7ecf3c3c691985560
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2025-11-03 10:16:03 +0100

    test speed on ios 26

--- Components/NavigationFooter/script.js
+++ Components/NavigationFooter/script.js
@@ -15,20 +15,15 @@ function initMarquee(refs) {
     slidesPerView: 'auto',
     spaceBetween: 90,
     loop: true,
-    speed: 50000,
+    speed: 8000,
     autoplay: {
-      delay: 0,
+      delay: 1,
       disableOnInteraction: false,
       pauseOnMouseEnter: false,
-      waitForTransition: false,
     },
     loopAdditionalSlides: 3,
-    allowTouchMove: true,
+    allowTouchMove: false,
     simulateTouch: false,
-    freeMode: {
-      enabled: true,
-      momentum: false,
-    },
   }
 
   return new Swiper(refs.marquee, config)
